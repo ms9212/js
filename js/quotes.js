c511143420be3598632d668013ab52f02051f59d
ms9212 <99110029+ms9212@users.noreply.github.com>
2022-12-12 18:28:45 +0900

    Create quotes.js

---
+++ js/quotes.js
@@ -0,0 +1,53 @@
+const quotes = [
+  {
+    quote: "When you have faults, do not fear to abandon them.",
+    author: "Confucius",
+  },
+  {
+    quote: "Age is no guarantee of maturity.",
+    author: "Lawana Blackwell",
+  },
+  {
+    quote:
+      "You will face many defeats in life, but never let yourself be defeated.",
+    author: "Maya Angelou",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "Life is either a daring adventure or nothing at all.",
+    author: "Helen Keller",
+  },
+  {
+    quote: "The goal of life is living in agreement with nature.",
+    author: "Zeno",
+  },
+  {
+    quote: "Only I can change me life, no one can do it for me.",
+    author: "Carol Burnett",
+  },
+  {
+    quote:
+      "All you need in this life is ignorance and confidence, then success is sure.",
+    author: "Mark Twain",
+  },
+  {
+    quote:
+      "Life can only be understood backwards; but it must be lived forwards.",
+    author: "Søren Kierkegaard",
+  },
+  {
+    quote: "Great minds have purposes, others have wishes.",
+    author: "Washington Irving",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
